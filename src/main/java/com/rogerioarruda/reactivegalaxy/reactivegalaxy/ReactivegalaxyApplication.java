2c5d4d49d9d64fa35223cfa0b2c2d8de8e2cd426
Rogério Arruda <roger_yusho2@hotmail.com>
2020-10-04 07:09:33 -0300

    First commit

---
+++ src/main/java/com/rogerioarruda/reactivegalaxy/reactivegalaxy/ReactivegalaxyApplication.java
@@ -0,0 +1,13 @@
+package com.rogerioarruda.reactivegalaxy.reactivegalaxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivegalaxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactivegalaxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rogerioarruda/reactivegalaxy/reactivegalaxy/ReactivegalaxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogerioarruda.reactivegalaxy.reactivegalaxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactivegalaxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
